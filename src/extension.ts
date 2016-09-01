f515c1ff309c28ceb89ab0d492f0bd36d05b764a
Pouya Kary <k@karyfoundation.org>
2016-09-01 12:06:25 +0430

    min...

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,23 @@
 
 //
+// Righteous for Visual Studio Code.
+//    Copyright 2016 by Kary Foundation. Inc.
+//    Author: Pouya Kary <k@karyfoundation.org>
+//
+
+//
 // ─── IMPORTS ────────────────────────────────────────────────────────────────────
 //
-    
+
     import * as vscode from 'vscode';
     import righteous = require( 'righteous' );
-    
+
 //
 // ─── FORMAT ─────────────────────────────────────────────────────────────────────
 //
-    
-    export function format ( document: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions ) {
 
+    export function format ( document: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions ) {
+    
         var start = new vscode.Position( 0, 0 );
         var end = new vscode.Position( document.lineCount - 1, document.lineAt( document.lineCount - 1 ).text.length );
         range = new vscode.Range( start, end );
@@ -32,7 +38,7 @@
     
         return result;
     };
-    
+
 //
 // ─── REGISTRATION ───────────────────────────────────────────────────────────────
 //
@@ -40,29 +46,28 @@
     // this method is called when your extension is activated
     // your extension is activated the very first time the command is executed
     export function activate ( context: vscode.ExtensionContext ) {
-		for ( let language of ['typescript', 'javascript'] ) {
-
-			context.subscriptions.push(
-				vscode.languages.registerDocumentFormattingEditProvider( language, {
-					provideDocumentFormattingEdits: ( document, options, token ) => {
-						return format( document, null, options )
-					}
-				})
-			);
-		
-			context.subscriptions.push(
-				vscode.languages.registerDocumentRangeFormattingEditProvider( language, {
-					provideDocumentRangeFormattingEdits: ( document, range, options, token ) => {
-						var start = new vscode.Position( 0, 0 );
-						var end = new vscode.Position( document.lineCount - 1, document.lineAt( document.lineCount - 1 ).text.length );
-						return format( document, new vscode.Range( start, end ), options )
-					}
-				})
-			);
-		}
-    }
+        for ( let language of [ 'typescript', 'javascript' ] ) {
     
+            context.subscriptions.push(
+                vscode.languages.registerDocumentFormattingEditProvider( language, {
+                    provideDocumentFormattingEdits: ( document, options, token ) => {
+                        return format( document, null, options )
+                    }
+                })
+            );
+    
+            context.subscriptions.push(
+                vscode.languages.registerDocumentRangeFormattingEditProvider( language, {
+                    provideDocumentRangeFormattingEdits: ( document, range, options, token ) => {
+                        var start = new vscode.Position( 0, 0 );
+                        var end = new vscode.Position( document.lineCount - 1, document.lineAt( document.lineCount - 1 ).text.length );
+                        return format( document, new vscode.Range( start, end ), options )
+                    }
+                })
+            );
+        }
+    }
+
 // ────────────────────────────────────────────────────────────────────────────────
 
-    
     
